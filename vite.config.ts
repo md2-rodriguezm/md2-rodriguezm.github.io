68f4f7f5335cbb8817158c71afe0e5b0285f978e
Luis Guillén Servera <luisgservsp@gmail.com>
2026-02-18 23:05:23 +0000

    build: Generate production build output and update build configuration files.

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // para usuario.github.io
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
+    hmr: { overlay: false },
   },
   plugins: [react()],
   assetsInclude: ["**/*.JPG"],
